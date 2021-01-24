4ef343c3be1fb002b6212e95ac9f8bdb8c2256cb
hanna kebede <se.hanna.kebede@gmail.com>
2021-01-24 11:56:47 +0300

    start bank system

--- BMI/assets/js/app.js
+++ BMI/assets/js/app.js
@@ -1,9 +1,24 @@
-var Height = parseInt(prompt("Enter your Height: "));
-var Weight = parseInt(prompt("Enter your Weight"));
+var Height = prompt("Enter your Height: ");
+var Weight = prompt("Enter your Weight: ");
 
 let calcBmi = function(Height, Weight) {
      
-    console.log("Your BMI is : " + Weight/(Height*Height) );
-
+    return (Weight / Math.pow(Height, 2)); 
 }
-parseInt(calcBmi());
+
+
+var bmi =  console.log("Your BMI is : " + calcBmi(Height, Weight ));
+
+// function undentify(){
+//     if (bmi < 18.5){
+//         return "you are under weight."
+//     }else if(bmi >= 18.5 & bmi <= 24.5 ){
+//         return "you are normal weight."
+//     }else if(bmi >= 25.0 & bmi <=29.9){
+//         return "you are over weighy."
+//     }else{
+//         return "you are obese"
+//     }
+// }
+// undentify();
+
