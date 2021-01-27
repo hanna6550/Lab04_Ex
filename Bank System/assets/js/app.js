1e6640c9284eecca89eb4b9eecd4ce527ea9094a
hanna kebede <se.hanna.kebede@gmail.com>
2021-01-27 18:57:07 +0300

    Bank System Final

--- Bank System/assets/js/app.js
+++ Bank System/assets/js/app.js
@@ -1,8 +1,8 @@
-var accounts = [100, 346, 789, 435]
+var accounts = [100, 500, 1000, 2000]
 
 function deposit(amount, accNum){
     accounts[accNum] += amount
-    console.log("Deposited to : " + accNum + " Deposited Amount: " + amount + " total balance : " + accounts[accNum])
+    console.log("Deposited to : " + accNum + ", " + " Deposited Amount: " + amount + ", " + " total balance : " + accounts[accNum])
     return true
 }
 
@@ -10,19 +10,19 @@ function withdraw(amount, accNum){
     if (amount < accounts[accNum]){
         if(amount <= 100){
             accounts[accNum] -= amount
-            console.log("Withdrawn from : "+ accNum + " Withdrawn Amount: " + amount + " total balance : " + accounts[accNum])
+            console.log("Withdrawn from : "+ accNum + ", " +  " Withdrawn Amount: " + amount + ", " +  " total balance : " + accounts[accNum])
             return true
         }else{
-            alert("you cannot withdraw more than $100")
+            alert("you cannot withdraw more than 100 birr!")
         }
     } else{
-        alert("No enough Money to withdraw")
+        alert("No enough Money to withdraw!")
     }
     return false
 }
 
 function balance(accNum){
-    alert("Your Balance is " + accounts[accNum])
+    alert("Your Balance is : " + accounts[accNum] +".")
 }
 
 function transfer(from, to, amount){
@@ -33,20 +33,21 @@ function transfer(from, to, amount){
 
 (
     function(){
-        service = parseInt(prompt("What do you want to do today: \n 1: Deposit \n 2: Withdraw \n 3: Inquire Balance \n 4: transfer"))
+        service = parseInt(prompt("What do you want to do today: \n 1: Deposit \n 2: Withdraw \n 3: Balance \n 4: Transfer"))
+        
         console.log(accounts)
         switch (service) {
             case 1:
-                deposit(parseInt(prompt("Enter amount: ")), parseInt(prompt("Enter account Number(between 0 and "+(accounts.length-1)+"): ")));
+                deposit(parseInt(prompt("Enter amount: ")), parseInt(prompt("Enter account Number (between 0 and "+(accounts.length-1)+"): ")));
                 break;
             case 2:
-                withdraw(parseInt(prompt("Enter amount: ")), parseInt(prompt("Enter account Number(between 0 and "+(accounts.length-1)+"): ")));
+                withdraw(parseInt(prompt("Enter amount: ")), parseInt(prompt("Enter account Number (between 0 and "+(accounts.length-1)+"): ")));
                 break;
             case 3:
                 balance(parseInt(prompt("Enter account Number(between 0 and "+(accounts.length-1)+"): ")));
                 break;
             case 4:
-                transfer(parseInt(prompt("Enter account to debt from Number(between 0 and "+(accounts.length-1)+"): ")),parseInt(prompt("Enter account to deposit into Number(between 0 and "+(accounts.length-1)+"): ")), parseInt(prompt("Enter amount: ")));
+                transfer(parseInt(prompt("Enter account to debt from Number (between 0 and "+(accounts.length-1)+"): ")),parseInt(prompt("Enter account to deposit into Number (between 0 and "+(accounts.length-1)+"): ")), parseInt(prompt("Enter amount: ")));
                 break;
             default:
                 alert("Invalid Input");
